a1d4ee6e142e1e5f3541126ca1e3959fe99eb7f3
AuWeerachai <auweerachais@gmail.com>
2026-01-22 19:33:19 -0500

    Ros2 added

--- strertch_ros.py
+++ strertch_ros.py
@@ -17,23 +17,18 @@ class SangStretchNode(hm.HelloNode):
         self.move_to_pose({'joint_arm': 0.5, 'joint_lift': 1.1}, blocking=True)
         
         self.move_to_pose({'joint_wrist_yaw': math.radians(45)}, blocking=True)
-
         self.move_to_pose({'joint_wrist_roll':  math.radians(45)}, blocking=True)
-        
         self.move_to_pose({'joint_wrist_pitch': math.radians(45)}, blocking=True)
         
-
 
         self.move_to_pose({'joint_gripper_finger_right': math.radians(45)}, blocking=True)
         self.move_to_pose({'joint_gripper_finger_right': math.radians(0)}, blocking=True)
 
 
-        self.move_to_pose({'joint_head_tilt': math.radians(90)}, blocking=True)
-
+        self.move_to_pose({'joint_head_tilt': math.radians(120)}, blocking=True)
         self.move_to_pose({'joint_head_pan':  math.radians(-90)}, blocking=True)
         
    
-
         self.stow_the_robot()
 
         self.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
