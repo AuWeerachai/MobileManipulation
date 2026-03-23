eb0369a8c120a5548752d426b938f3a8a4b1c1b5
AuWeerachai <auweerachais@gmail.com>
2026-03-23 17:58:32 -0400

    update point lab4

--- Lab1/stretch_api.py
+++ Lab1/stretch_api.py
@@ -5,56 +5,56 @@ import numpy as np
 #start and reset
 robot = stretch_body.robot.Robot()
 robot.startup()
-robot.stow()
-robot.push_command()
-robot.wait_command()
+# robot.stow()
+# robot.push_command()
+# robot.wait_command()
 
 
 #move lift/arm
-robot.arm.move_to(0.5) # Move telescoping arm
-robot.lift.move_to(1.1) # Move lift
-robot.push_command()
-robot.wait_command()
+# robot.arm.move_to(0.5) # Move telescoping arm
+# robot.lift.move_to(1.1) # Move lift
+# robot.push_command()
+# robot.wait_command()
 
 #move wrist
-robot.end_of_arm.move_to('wrist_yaw', np.radians(0))
-robot.push_command()
-robot.wait_command()
-robot.end_of_arm.move_to('wrist_pitch', np.radians(0))
-robot.push_command()
-robot.wait_command()
-robot.end_of_arm.move_to('wrist_roll', np.radians(90))
-robot.push_command()
-robot.wait_command()
-robot.end_of_arm.move_to('stretch_gripper', 50)
-robot.push_command()
-robot.wait_command()
-robot.end_of_arm.move_to('stretch_gripper', 0)
-robot.push_command()
-robot.wait_command()
+# robot.end_of_arm.move_to('wrist_yaw', np.radians(0))
+# robot.push_command()
+# robot.wait_command()
+# robot.end_of_arm.move_to('wrist_pitch', np.radians(0))
+# robot.push_command()
+# robot.wait_command()
+# robot.end_of_arm.move_to('wrist_roll', np.radians(90))
+# robot.push_command()
+# robot.wait_command()
+# robot.end_of_arm.move_to('stretch_gripper', 50)
+# robot.push_command()
+# robot.wait_command()
+# robot.end_of_arm.move_to('stretch_gripper', 0)
+# robot.push_command()
+# robot.wait_command()
 
 #move camera
 robot.head.move_by('head_pan', np.radians(90))
 robot.push_command()
 robot.wait_command()
-robot.head.move_by('head_tilt', np.radians(-90))
-robot.push_command()
-robot.wait_command()
+# robot.head.move_by('head_tilt', np.radians(-90))
+# robot.push_command()
+# robot.wait_command()
 
-#robot stow
-robot.stow()
-robot.push_command()
-robot.wait_command()
+# #robot stow
+# robot.stow()
+# robot.push_command()
+# robot.wait_command()
 
-#move base
-robot.base.translate_by(0.5) # Move robot base 0.5 meters forward
-robot.push_command()
-robot.wait_command()
-robot.base.rotate_by(np.radians(180))
-robot.push_command()
-robot.wait_command()
-robot.base.translate_by(0.5) # Move robot base 0.5 meters forward
-robot.push_command()
-robot.wait_command()
+# #move base
+# robot.base.translate_by(0.5) # Move robot base 0.5 meters forward
+# robot.push_command()
+# robot.wait_command()
+# robot.base.rotate_by(np.radians(180))
+# robot.push_command()
+# robot.wait_command()
+# robot.base.translate_by(0.5) # Move robot base 0.5 meters forward
+# robot.push_command()
+# robot.wait_command()
 
-robot.stop()
+# robot.stop()
